45d9624a8d52cb593db7bac0b68b35b7edd47f35
Eduardito <ecaflimaperu@gmail.com>
2025-05-13 17:59:53 -0500

    modal agregado a cada image

--- js.js
+++ js.js
@@ -16,3 +16,25 @@ document.getElementById("scrollToTopBtn").addEventListener("click", function () 
     behavior: "smooth"
   });
 });
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Cerrar al hacer clic fuera de la imagen
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
